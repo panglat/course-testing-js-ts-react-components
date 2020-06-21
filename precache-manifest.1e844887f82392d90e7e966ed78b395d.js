fca69012f0ef57fc58f021701efb3ad8d3c61ad1
Pablo Anglat <pablo.anglat@globant.com>
2020-06-21 00:25:47 -0300

    Updates

---
+++ precache-manifest.1e844887f82392d90e7e966ed78b395d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1e7dd4ec4ecf793e9cb297bcf03a02",
+    "url": "/course-testing-js-ts-react-components/index.html"
+  },
+  {
+    "revision": "d7876caa9e82b6c6e1d8",
+    "url": "/course-testing-js-ts-react-components/static/css/main.3afa92ee.chunk.css"
+  },
+  {
+    "revision": "999606b46f5233539cd4",
+    "url": "/course-testing-js-ts-react-components/static/js/2.d35afeb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/course-testing-js-ts-react-components/static/js/2.d35afeb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7876caa9e82b6c6e1d8",
+    "url": "/course-testing-js-ts-react-components/static/js/main.0bc67f41.chunk.js"
+  },
+  {
+    "revision": "41213a8eb7b8700e19da",
+    "url": "/course-testing-js-ts-react-components/static/js/runtime-main.c4503925.js"
+  }
+]);
