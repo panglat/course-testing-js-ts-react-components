d546020d6b4688588d34bad6c8b4402286e0f953
Pablo Anglat <pablo.anglat@globant.com>
2020-06-24 22:32:00 -0300

    Use rerender and queryByRole

--- src/components/FavoriteNumber/index.test.tsx
+++ src/components/FavoriteNumber/index.test.tsx
@@ -10,8 +10,12 @@ test('renders a number input with a label "Favorite Number"', () => {
 });
 
 test('entering an invalid value shows an error message', () => {
-  const { getByLabelText, getByRole } = render(<FavoriteNumber />);
+  const { getByLabelText, getByRole, queryByRole, rerender } = render(
+    <FavoriteNumber />
+  );
   const input = getByLabelText(/favorite number/i);
   user.type(input, '10');
   expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i);
+  rerender(<FavoriteNumber max={10} />);
+  expect(queryByRole('alert')).toBeNull();
 });
