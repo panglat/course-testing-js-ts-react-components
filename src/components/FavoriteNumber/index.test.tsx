35c8912d97e448745c9d4bb56c2516485ae5b978
Pablo Anglat <pablo.anglat@globant.com>
2020-06-24 22:21:46 -0300

    Fix quotes in text

--- src/components/FavoriteNumber/index.test.tsx
+++ src/components/FavoriteNumber/index.test.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { render, fireEvent } from "@testing-library/react";
-import FavoriteNumber from ".";
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import FavoriteNumber from '.';
 
 test('renders a number input with a label "Favorite Number"', () => {
   const { getByLabelText } = render(<FavoriteNumber />);
   const input = getByLabelText(/favorite number/i);
-  expect(input).toHaveAttribute("type", "number");
+  expect(input).toHaveAttribute('type', 'number');
 });
 
-test("entering an invalid value shows an error message", () => {
+test('entering an invalid value shows an error message', () => {
   const { getByLabelText, getByRole } = render(<FavoriteNumber />);
   const input = getByLabelText(/favorite number/i);
-  fireEvent.change(input, { target: { value: "10" } });
-  expect(getByRole("alert")).toHaveTextContent(/the number is invalid/i);
+  fireEvent.change(input, { target: { value: '10' } });
+  expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i);
 });
