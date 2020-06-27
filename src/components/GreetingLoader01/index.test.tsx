28744f6d27a42b4d69ace505393267f6f0109c65
Pablo Anglat <pablo.anglat@globant.com>
2020-06-26 23:24:45 -0300

    13.Mock HTTP Requests with Dependency Injection in React Component Tests

--- src/components/GreetingLoader01/index.test.tsx
+++ src/components/GreetingLoader01/index.test.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
 import { render, fireEvent, wait } from '@testing-library/react';
-import { loadGreeting as mockLoadGreeting } from 'api';
 import { Greeting } from 'models/Greeting';
 
 import GreetingLoader from '.';
 
+/*
+import { loadGreeting as mockLoadGreeting } from 'api';
+
 jest.mock('api');
 
 test('loads greetings on click', async () => {
+  const mockLoadGreeting = jest.fn();
   const testGreeting = 'TEXT_GREETING';
   (mockLoadGreeting as jest.Mock<Promise<Greeting>>).mockResolvedValueOnce({
     data: { greeting: testGreeting },
   });
   const { getByLabelText, getByText } = render(<GreetingLoader />);
+  const nameInput = getByLabelText(/name/i) as HTMLInputElement;
+  const loadButton = getByText(/load/i);
+  nameInput.value = 'Mary';
+  fireEvent.click(loadButton);
+  expect(mockLoadGreeting).toHaveBeenCalledWith('Mary');
+  expect(mockLoadGreeting).toHaveBeenCalledTimes(1);
+  await wait(() =>
+    expect(getByLabelText(/greeting/i)).toHaveTextContent(testGreeting)
+  );
+});
+*/
+
+test('loads greetings on click', async () => {
+  const mockLoadGreeting = jest.fn<Promise<Greeting>, any>();
+  const testGreeting = 'TEXT_GREETING';
+  mockLoadGreeting.mockResolvedValueOnce({ data: { greeting: testGreeting } });
+  const { getByLabelText, getByText } = render(
+    <GreetingLoader loadGreeting={mockLoadGreeting} />
+  );
   const nameInput = getByLabelText(/name/i) as HTMLInputElement;
   const loadButton = getByText(/load/i);
   nameInput.value = 'Mary';
--- src/components/GreetingLoader01/index.tsx
+++ src/components/GreetingLoader01/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import * as api from 'api';
 
 import './styles.scss';
-import { loadGreeting } from 'api';
+interface GreetingLoader01Props {
+  loadGreeting: jest.Mock<
+    Promise<{
+      data: {
+        greeting: string;
+      };
+    }>
+  >;
+}
 
-const GreetingLoader01: React.FC = () => {
+const GreetingLoader01: React.FC<GreetingLoader01Props> = ({
+  loadGreeting = api.loadGreeting,
+}) => {
   const [greeting, setGreeting] = React.useState('');
   async function loadGreetingForInput(
     e: React.FormEvent<HTMLFormElement> | any
   ) {
     e.preventDefault();
-    const { data } = await loadGreeting(e.target.elements.name.value);
+    const value = e.target.elements.name.value as string;
+    const { data } = await loadGreeting(value);
     setGreeting(data.greeting);
   }
   return (
