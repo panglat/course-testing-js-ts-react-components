ca549b8853035d08f6f46bdc82177b1df8e5a9ab
Pablo Anglat <pablo.anglat@globant.com>
2020-06-29 23:42:34 -0300

    16.Hide console.error Logs when Testing Error Boundaries with jest.spyOn

--- src/components/ErrorBoundary/index.test.tsx
+++ src/components/ErrorBoundary/index.test.tsx
@@ -5,8 +5,17 @@ import { reportError as mockReportError } from 'api';
 
 jest.mock('api');
 
-//beforeEach(() => const mockReportError2 =
+let spy: jest.SpyInstance;
+let mockReportErrorTypes: jest.Mock;
+
+beforeEach(() => {
+  spy = jest.spyOn(console, 'error').mockImplementation(() => {});
+  mockReportErrorTypes = mockReportError as jest.Mock;
+});
 afterEach(() => jest.clearAllMocks());
+afterAll(() => {
+  spy.mockRestore();
+});
 
 function Bomb({ shouldThrown }: { shouldThrown?: boolean }) {
   if (shouldThrown) {
@@ -17,7 +26,7 @@ function Bomb({ shouldThrown }: { shouldThrown?: boolean }) {
 }
 
 test('calls reportError and renders that there was a problem', () => {
-  (mockReportError as jest.Mock).mockResolvedValueOnce({ success: true });
+  mockReportErrorTypes.mockResolvedValueOnce({ success: true });
   const { rerender } = render(
     <ErrorBoundary>
       <Bomb />
@@ -34,4 +43,6 @@ test('calls reportError and renders that there was a problem', () => {
   const info = { componentStack: expect.stringContaining('Bomb') };
   expect(mockReportError).toHaveBeenCalledWith(error, info);
   expect(mockReportError).toHaveBeenCalledTimes(1);
+
+  expect(console.error).toHaveBeenCalledTimes(2);
 });
