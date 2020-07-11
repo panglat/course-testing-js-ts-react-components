d184419ba11b6f0e20355a0bfe6bfa1b4a36a251
Pablo Anglat <pablo.anglat@globant.com>
2020-07-11 00:12:44 -0300

    29.Create a Custom render Function to Simplify Tests of react-router Components

--- src/components/Main/index.test.tsx
+++ src/components/Main/index.test.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import React, { ReactElement } from 'react';
+import { render as rtlRender, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Main from '.';
 
+function render(ui: ReactElement, { route = '/', ...renderOptions } = {}) {
+  const Wrapper: React.FC = ({ children }) => {
+    return <MemoryRouter initialEntries={[route]}>{children}</MemoryRouter>;
+  };
+
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
 test('main renders about and home and I can navigate to those pages', () => {
-  const { getByRole, getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <Main />
-    </MemoryRouter>
-  );
+  const { getByRole, getByText } = render(<Main />);
   expect(getByRole('heading')).toHaveTextContent(/home/i);
   fireEvent.click(getByText(/about/i));
   expect(getByRole('heading')).toHaveTextContent(/about/i);
 });
 
 test('landing on a bad page shows no match component', () => {
-  const { getByRole } = render(
-    <MemoryRouter initialEntries={['/something-that-does-not-match']}>
-      <Main />
-    </MemoryRouter>
-  );
+  const { getByRole } = render(<Main />, {
+    route: '/something-that-does-not-match',
+  });
   expect(getByRole('heading')).toHaveTextContent(/404/i);
 });
