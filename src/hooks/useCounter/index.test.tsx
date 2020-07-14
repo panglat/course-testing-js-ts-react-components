7bd71c01a90b23ad8c7fd11e3b2c46e263b4853f
Pablo Anglat <pablo.anglat@globant.com>
2020-07-14 19:34:22 -0300

    35.Test a Custom React Hook with renderHook from React Hooks Testing Library

--- src/hooks/useCounter/index.test.tsx
+++ src/hooks/useCounter/index.test.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import { render, act } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react-hooks';
 import useCounter from '.';
 
-function setup({ initialProps }: any = {}) {
-  const result: any = {};
-  function TestComponent(props: any) {
-    result.current = useCounter(props);
-    return null;
-  }
-  render(<TestComponent {...initialProps} />);
-  return result;
-}
-
 test('exposes the count and increment/decrement functions', () => {
-  const result = setup();
+  const { result } = renderHook(useCounter);
   expect(result.current.count).toBe(0);
   act(() => result.current.increment());
   expect(result.current.count).toBe(1);
@@ -22,12 +12,14 @@ test('exposes the count and increment/decrement functions', () => {
 });
 
 test('allows customization of the initial count', () => {
-  const result = setup({ initialProps: { initialCount: 3 } });
+  const { result } = renderHook(useCounter, {
+    initialProps: { initialCount: 3 },
+  });
   expect(result.current.count).toBe(3);
 });
 
 test('allows customization of the step', () => {
-  const result = setup({ initialProps: { step: 2 } });
+  const { result } = renderHook(useCounter, { initialProps: { step: 2 } });
   expect(result.current.count).toBe(0);
   act(() => result.current.increment());
   expect(result.current.count).toBe(2);
